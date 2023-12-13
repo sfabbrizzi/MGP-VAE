adeb7a9ae6e4a7d06eef12403e56d0b04a1281b6
sfabbrizzi <simone.fabbrizzi@gmail.com>
2023-12-13 12:14:51 +0100

    PEP8 compliance

--- style_transfer_across_videos.py
+++ style_transfer_across_videos.py
@@ -1,81 +1,143 @@
 import os
 import torch
-import matplotlib
-matplotlib.use('agg')
-import matplotlib.pyplot as plt
-import numpy as np
-from os import listdir
-from flags import *
+
+from flags import (BATCH_SIZE,
+                   ENCODER_SAVE,
+                   DECODER_SAVE,
+                   CUDA,
+                   NUM_FEA,
+                   FEA_DIM,
+                   NUM_FRAMES,
+                   NUM_INPUT_CHANNELS,
+                   NDIM)
 from networks import Encoder, Decoder
+from dataloader import load_dataset
 from itertools import cycle
 from torchvision.utils import save_image
 from torch.utils.data import DataLoader
 from utils import weights_init
-from dataloader import data_moving_mnist, data_dsprites
+
+import matplotlib
+matplotlib.use('agg')
 
 if __name__ == "__main__":
-	
-	if not os.path.exists('./results/style_transfer_results'):
-		os.makedirs('./results/style_transfer_results')
 
-	dataset = load_dataset()
-	loader = cycle(DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, drop_last=True))
+    if not os.path.exists('./results/style_transfer_results'):
+        os.makedirs('./results/style_transfer_results')
 
-	encoder = Encoder()
-	encoder.apply(weights_init)
+    dataset = load_dataset()
+    loader = cycle(DataLoader(dataset,
+                              batch_size=BATCH_SIZE,
+                              shuffle=True,
+                              drop_last=True))
 
-	decoder = Decoder()
-	decoder.apply(weights_init)
-			
-	encoder.load_state_dict(torch.load(os.path.join('checkpoints/', ENCODER_SAVE)))
-	decoder.load_state_dict(torch.load(os.path.join('checkpoints/', DECODER_SAVE)))
+    encoder = Encoder()
+    encoder.apply(weights_init)
 
-	encoder.eval().cuda()
-	decoder.eval().cuda()
-	
-	video1 = next(loader).float().cuda()[0].unsqueeze(0)
-	video2 = next(loader).float().cuda()[0].unsqueeze(0)
-	
-	X1, KL1, muL1, det_q1 = encoder(video1)
-	X2, KL2, muL2, det_q2 = encoder(video2)
+    decoder = Decoder()
+    decoder.apply(weights_init)
 
-	# save reconstructed images
-	dec_v1 = decoder(X1)
-	save_image(dec_v1.squeeze(0).transpose(2, 3), './results/style_transfer_results/recon_v1.png', nrow=NUM_FRAMES, normalize=True)
+    encoder.load_state_dict(torch.load(os.path.join('checkpoints/',
+                                                    ENCODER_SAVE)))
+    decoder.load_state_dict(torch.load(os.path.join('checkpoints/',
+                                                    DECODER_SAVE)))
 
-	dec_v2 = decoder(X2)
-	save_image(dec_v2.squeeze(0).transpose(2, 3), './results/style_transfer_results/recon_v2.png', nrow=NUM_FRAMES, normalize=True)
+    encoder.eval()
+    decoder.eval()
 
-	v1_feature = []
-	v2_feature = []
+    video1 = next(loader).float()[0].unsqueeze(0)
+    video2 = next(loader).float()[0].unsqueeze(0)
 
-	for i in range(NUM_FEA):
-		
-		v1_feature.append(X1[:, :, i*FEA_DIM:(i+1)*FEA_DIM])
-		v2_feature.append(X2[:, :, i*FEA_DIM:(i+1)*FEA_DIM])
+    if (CUDA and torch.cuda.is_available()):
+        encoder.cuda()
+        decoder.cuda()
 
-	for i in range(NUM_FEA):
+        video1 = video1.cuda()
+        video2 = video2.cuda()
+    elif (CUDA and torch.backends.mps.is_available()):
+        encoder = encoder.to("mps")
+        decoder = decoder.to("mps")
 
-		for j in range(NUM_FEA):
+        video1 = video1.to("mps")
+        video2 = video2.to("mps")
 
-			# style transfer on video1
-			v1_feature_transferred = torch.zeros(NUM_INPUT_CHANNELS, NUM_FRAMES, NDIM).cuda()
-			if (j == i):
-				v1_feature_transferred[:, :, i*FEA_DIM:(i+1)*FEA_DIM] = v2_feature[j]
-			else:
-				v1_feature_transferred[:, :, i*FEA_DIM:(i+1)*FEA_DIM] = v1_feature[j]
+    X1, KL1, muL1, det_q1 = encoder(video1)
+    X2, KL2, muL2, det_q2 = encoder(video2)
 
-			v1_feature_transferred_dec = decoder(v1_feature_transferred)
-			save_image(v1_feature_transferred_dec.squeeze(0).transpose(2, 3), './results/style_transfer_results/v1_grid_feature{}_transferred.png'.format(j), nrow=NUM_FRAMES, normalize=True)
+    # save reconstructed images
+    dec_v1 = decoder(X1)
+    save_image(dec_v1.squeeze(0).transpose(2, 3),
+               './results/style_transfer_results/recon_v1.png',
+               nrow=NUM_FRAMES,
+               normalize=True)
 
-			# style transfer on video2
-			v2_feature_transferred = torch.zeros(NUM_INPUT_CHANNELS, NUM_FRAMES, NDIM).cuda()
-			if (j == i):
-				v2_feature_transferred[:, :, i*FEA_DIM:(i+1)*FEA_DIM] = v1_feature[j]
-			else:
-				v2_feature_transferred[:, :, i*FEA_DIM:(i+1)*FEA_DIM] = v2_feature[j]
-			
-			v2_feature_transferred_dec = decoder(v2_feature_transferred)
-			save_image(v2_feature_transferred_dec.squeeze(0).transpose(2, 3), './results/style_transfer_results/v2_grid_feature{}_transferred.png'.format(j), nrow=NUM_FRAMES, normalize=True)
+    dec_v2 = decoder(X2)
+    save_image(dec_v2.squeeze(0).transpose(2, 3),
+               './results/style_transfer_results/recon_v2.png',
+               nrow=NUM_FRAMES,
+               normalize=True)
 
-	
+    v1_feature = []
+    v2_feature = []
+
+    for i in range(NUM_FEA):
+
+        v1_feature.append(X1[:, :, i*FEA_DIM:(i+1)*FEA_DIM])
+        v2_feature.append(X2[:, :, i*FEA_DIM:(i+1)*FEA_DIM])
+
+    for i in range(NUM_FEA):
+
+        for j in range(NUM_FEA):
+
+            # style transfer on video1
+            v1_feature_transferred = torch.zeros(NUM_INPUT_CHANNELS,
+                                                 NUM_FRAMES,
+                                                 NDIM)
+
+            if (CUDA and torch.cuda.is_available()):
+                v1_feature_transferred = v1_feature_transferred.cuda()
+
+            elif (CUDA and torch.backends.mps.is_available()):
+                v1_feature_transferred = v1_feature_transferred.to("mps")
+
+            if (j == i):
+                v1_feature_transferred[:,
+                                       :,
+                                       i*FEA_DIM:(i+1)*FEA_DIM] = v2_feature[j]
+            else:
+                v1_feature_transferred[:,
+                                       :,
+                                       i*FEA_DIM:(i+1)*FEA_DIM] = v1_feature[j]
+
+            v1_feature_transferred_dec = decoder(v1_feature_transferred)
+            save_image(
+                v1_feature_transferred_dec.squeeze(0).transpose(2, 3),
+                ('./results/style_transfer_results/'
+                 'v1_grid_feature{}_transferred.png').format(j),
+                nrow=NUM_FRAMES, normalize=True)
+
+            # style transfer on video2
+            v2_feature_transferred = torch.zeros(NUM_INPUT_CHANNELS,
+                                                 NUM_FRAMES,
+                                                 NDIM)
+            if (CUDA and torch.cuda.is_available()):
+                v2_feature_transferred = v2_feature_transferred.cuda()
+
+            elif (CUDA and torch.backends.mps.is_available()):
+                v2_feature_transferred = v2_feature_transferred.to("mps")
+
+            if (j == i):
+                v2_feature_transferred[:,
+                                       :,
+                                       i*FEA_DIM:(i+1)*FEA_DIM] = v1_feature[j]
+            else:
+                v2_feature_transferred[:,
+                                       :,
+                                       i*FEA_DIM:(i+1)*FEA_DIM] = v2_feature[j]
+
+            v2_feature_transferred_dec = decoder(v2_feature_transferred)
+            save_image(v2_feature_transferred_dec.squeeze(0).transpose(2, 3),
+                       ('./results/style_transfer_results/'
+                        'v2_grid_feature{}_transferred.png').format(j),
+                       nrow=NUM_FRAMES,
+                       normalize=True)
--- videowise_visualization.py
+++ videowise_visualization.py
@@ -1,80 +1,101 @@
-# this file contains code that helps visualize the latent space encodings 
-# NOTE: this is valid only when all features are kept 2-dimensional (i.e. FEA_DIM == 2)
+# this file contains code that helps visualize the latent space encodings
+# note: this is valid only when all features are kept 2-dimensional
+# (i.e. FEA_DIM == 2)
 
 import os
-from os import listdir
 import torch
-import torch.nn as nn
-import numpy as np
 from itertools import cycle
-import matplotlib
-matplotlib.use('agg')
+
 import matplotlib.pyplot as plt
-from sklearn.manifold import TSNE
-from flags import *
+from flags import (BATCH_SIZE,
+                   ENCODER_SAVE,
+                   DECODER_SAVE,
+                   NUM_FEA,
+                   NUM_POINTS_VISUALIZATION,
+                   CUDA)
 from networks import Encoder, Decoder
 
-from torchvision.utils import save_image
 from torch.utils.data import DataLoader
 from utils import weights_init
-from dataloader import data_moving_mnist, data_dsprites, data_dsprites_color
+from dataloader import load_dataset
+
+import matplotlib
+matplotlib.use('agg')
+
 
 def choose_color(i):
+    # size of this list should be same as number of videos to be plotted
+    # (i.e. NUM_POINTS_VISUALIZATION)
+    colors = ['r', 'g', 'b', 'y', 'm', 'c']
+    # Add more colors here if you wish to plot more videos
 
-	# size of this list should be same as number of videos to be plotted (i.e. NUM_POINTS_VISUALIZATION)
-	colors = ['r', 'g', 'b', 'y', 'm', 'c'] 
-	# Add more colors here if you wish to plot more videos
+    return colors[i]
 
-	return colors[i]
 
 if __name__ == "__main__":
-	
-	if not os.path.exists('./results/video_visualization/'):
-		os.makedirs('./results/video_visualization/')
 
-	dataset = load_dataset()
-	loader = cycle(DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, drop_last=True))
+    if not os.path.exists('./results/video_visualization/'):
+        os.makedirs('./results/video_visualization/')
 
-	encoder = Encoder()
-	encoder.apply(weights_init)
+    dataset = load_dataset()
+    loader = cycle(DataLoader(dataset,
+                              batch_size=BATCH_SIZE,
+                              shuffle=True,
+                              drop_last=True))
 
-	decoder = Decoder()
-	decoder.apply(weights_init)
-			
-	encoder.load_state_dict(torch.load(os.path.join('checkpoints/', ENCODER_SAVE)))
-	decoder.load_state_dict(torch.load(os.path.join('checkpoints/', DECODER_SAVE)))
+    encoder = Encoder()
+    encoder.apply(weights_init)
 
-	encoder.eval().cuda()
-	decoder.eval().cuda()
+    decoder = Decoder()
+    decoder.apply(weights_init)
 
-	for i in range(NUM_FEA):
+    encoder.load_state_dict(torch.load(os.path.join('checkpoints/',
+                                                    ENCODER_SAVE)))
+    decoder.load_state_dict(torch.load(os.path.join('checkpoints/',
+                                                    DECODER_SAVE)))
 
-		fea_x = []
-		fea_y = []
+    encoder.eval()
+    decoder.eval()
 
-		for j in range(NUM_POINTS_VISUALIZATION):
+    if (CUDA and torch.cuda.is_available()):
+        encoder.cuda()
+        decoder.cuda()
+    elif (CUDA and torch.backends.mps.is_available()):
+        encoder.to("mps")
+        decoder.to("mps")
 
-			X_in = next(loader)
-			X_in = X_in.float().cuda()
+    for i in range(NUM_FEA):
 
-			X1, KL1, muL1, det_q1 = encoder(X_in)
-			X1 = X1.data.cpu().numpy()
+        fea_x = []
+        fea_y = []
 
-			fea_x.append(X1[:, :, 2*i])
-			fea_y.append(X1[:, :, 2*i + 1])
+        for j in range(NUM_POINTS_VISUALIZATION):
 
-		fig, ax = plt.subplots(1)
-		
-		for j in range(NUM_POINTS_VISUALIZATION):
-			
-			# plottings video encodings with different color for each video
-			plt.scatter(fea_x[j], fea_y[j], marker='o', c=choose_color(j), cmap=plt.cm.get_cmap("jet", 10), edgecolor='k')
-		
-		# may vary the limits of this figure depending on the spread of each Gaussian process
-		plt.xlim(-3, 3)
-		plt.ylim(-3, 3)
-		plt.savefig("./results/video_visualization/fea{}.png".format(str(i)))
-		
-		plt.close()
+            X_in = next(loader)
+            X_in = X_in.float().cuda()
 
-		
+            X1, KL1, muL1, det_q1 = encoder(X_in)
+            X1 = X1.data.cpu().numpy()
+
+            fea_x.append(X1[:, :, 2*i])
+            fea_y.append(X1[:, :, 2*i + 1])
+
+        fig, ax = plt.subplots(1)
+
+        for j in range(NUM_POINTS_VISUALIZATION):
+
+            # plottings video encodings with different color for each video
+            plt.scatter(fea_x[j],
+                        fea_y[j],
+                        marker='o',
+                        c=choose_color(j),
+                        cmap=plt.cm.get_cmap("jet", 10),
+                        edgecolor='k')
+
+        # may vary the limits of this figure depending on the
+        # spread of each Gaussian process
+        plt.xlim(-3, 3)
+        plt.ylim(-3, 3)
+        plt.savefig("./results/video_visualization/fea{}.png".format(str(i)))
+
+        plt.close()
